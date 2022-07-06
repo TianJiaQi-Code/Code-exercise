a5115baf3973cf2289956d25ad9732c421e1f171
Tianbottom <108409193+Tianbottom@users.noreply.github.com>
2022-07-06 22:04:25 +0800

    Add files via upload

--- test_6_30/test.c
+++ test_6_30/test.c
@@ -1,9 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+//数据的存储练习
+#include<stdio.h>
 
 
-int main()
-{
-	int a = 10;
-	float f = 10.0;
-	return 0;
-}
+
+//int main()
+//{
+//	//写一段代码告诉我们当前机器的字节序是什么
+//	int a = 1;
+//	char* p = (char*)&a;
+//	if (*p == 1)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	float f = 10.0;
+//	return 0;
+//}
