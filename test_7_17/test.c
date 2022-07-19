45edf9ca56b04a7e7d320f9cf07b0e11e0f73f03
Tianbottom <108409193+Tianbottom@users.noreply.github.com>
2022-07-19 20:48:12 +0800

    Add files via upload

--- test_7_17/test.c
+++ test_7_17/test.c
@@ -2,6 +2,20 @@
 
 #include<stdio.h>
 
+
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int(*pa)[10] = &arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", (*pa)[i]);
+//	}
+//	return 0;
+//}
+
 //int main()
 //{
 //	int arr1[] = { 1,2,3,4,5 };
