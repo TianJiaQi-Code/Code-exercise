d2996e7cae14af885f1b96f3ba0c4a637885e61a
Tianbottom <108409193+Tianbottom@users.noreply.github.com>
2022-07-28 19:34:55 +0800

    Add files via upload

--- test_7_27/test.c
+++ test_7_27/test.c
@@ -1,6 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+#include<stdio.h>
+
+//0x100000 0x100014
+//0x100001 0x100004
+
+//2,5 
+
 //7 4/8 1 1 4/8 4/8 4/8
 //6 6 err err 6 随机值 5
 //4/8 4/8 1 1 4/8 4/8 4/8
 //6 5 err err 随机值 随机值 5
+//48 4 16 4/8 4 4/8 16 4/8 16 16 16
+
+//int main()
+//{
+//	int a[3][4] = { 0 };
+//	printf("%d\n", sizeof(a[3]));
+//	return 0;
+//}
