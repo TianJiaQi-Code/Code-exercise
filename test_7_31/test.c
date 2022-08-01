2e572cf58dc02efed01f21fa381ef35500a56f7a
Tianbottom <108409193+Tianbottom@users.noreply.github.com>
2022-08-01 20:25:42 +0800

    Add files via upload

--- test_7_31/test.c
+++ test_7_31/test.c
@@ -1,5 +1,108 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+#include<string.h>
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int line = 0;
+	scanf("%d", &line);
+	//打印上半部分
+	int i = 0;
+	for (i = 0; i < line; i++)
+	{
+		//打印空格
+		int j = 0;
+		for (j = 0; j < line - 1 - i; j++)
+		{
+			printf(" ");
+		}
+		//打印*
+		for (j = 0; j < 2 * i + 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	//打印下半部分
+	for (i = 0; i < line - 1 ; i++)
+	{
+		int j = 0;
+		//打印空格
+		for (j = 0; j <= i; j++)
+		{
+			printf(" ");
+		}
+		//打印*
+		for (j = 0; j < 2*(line-1-i)-1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i <= 100000; i++)
+//	{
+//		//判断i是否为水仙花数（自幂数）
+//		//1. 计算i的位数 - n位数
+//		int n = 1;
+//		int tmp = i;
+//		int sum = 0;
+//		while (tmp /=  10)
+//		{
+//			n++;
+//		}
+//		//123/10 n++
+//		//12/10 n++
+//		//1/10
+//
+//		//2. 计算i的每一位的n次方之和 sum
+//		tmp = i;
+//		while (tmp)
+//		{
+//			sum += pow(tmp % 10, n);
+//			tmp /= 10;
+//		}
+//		//3. 比较i == sum
+//		if (i == sum)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+//void nixv(char* str)
+//{
+//	int len = strlen(str);
+//	char* left = str;
+//	char* right = str + len - 1;
+//	while (left<right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//	
+//}
+//
+//int main()
+//{
+//	char arr[100] = { 0 };
+//	gets(arr);
+//	nixv(arr);
+//	printf("%s ", arr);
+//	return 0;
+//}
+
 //10,5
 //at
 //POINT ER ST EW 
