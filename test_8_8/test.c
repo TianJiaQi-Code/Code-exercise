ec13b402927eda4c88e4c563333bbc17c13e326f
Tianbottom <108409193+Tianbottom@users.noreply.github.com>
2022-08-08 20:51:55 +0800

    Add files via upload

---
+++ test_8_8/test.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+
+
+
+//int is_left_move(char* str1, char* str2)
+//{
+//	int len1 = strlen(str1);
+//	int len2 = strlen(str2);
+//	if (len1 != len2)
+//	{
+//		return 0;
+//	}
+//	strncat(str1, str2, 6);
+//	char* ret = strstr(str1, str2);
+//	if (ret == NULL)
+//	{
+//		return 0;
+//	}
+//	else
+//	{
+//		return 1;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr1[30] = "abcdef";
+//	char arr2[] = "defab";
+//	int ret = is_left_move(arr1, arr2);
+//	if (ret == 1)
+//	{
+//		printf("Yes\n");
+//	}
+//	else
+//	{
+//		printf("No\n");
+//	}
+//	return 0;
+//}
