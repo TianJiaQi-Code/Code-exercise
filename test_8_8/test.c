1ea16d02292f132ddf5a90332dd7d75565722717
Tianbottom <108409193+Tianbottom@users.noreply.github.com>
2022-08-09 22:59:14 +0800

    Add files via upload

--- test_8_8/test.c
+++ test_8_8/test.c
@@ -2,8 +2,30 @@
 
 #include<stdio.h>
 #include<string.h>
+#include<assert.h>
 
 
+
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	char* ret = dest;
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr1[] = "abcdefghi";
+//	char arr2[] = "bit";
+//	my_strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
 
 //int is_left_move(char* str1, char* str2)
 //{
