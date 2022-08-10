3c4cef9838b2854e1533ba1da2e63018728203cb
Tianbottom <108409193+Tianbottom@users.noreply.github.com>
2022-08-10 19:14:02 +0800

    Add files via upload

--- test_8_8/test.c
+++ test_8_8/test.c
@@ -6,6 +6,33 @@
 
 
 
+//my_strcmp(const char* str1, const char* str2)
+//{
+//	assert(str1 && str2);
+//	while (*str1 == *str2)
+//	{
+//		if (*str1 == '\0')
+//		{
+//			return 0;
+//		}
+//		str1++;
+//		str2++;
+//	}
+//	if (*str1 > *str2)
+//		return 1;
+//	else
+//		return -1;
+//}
+//
+//int main()
+//{
+//	char* p1 = "abcdef";
+//	char* p2 = "abqwe";
+//	int ret = my_strcmp(p1, p2);
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
+
 //char* my_strcpy(char* dest, const char* src)
 //{
 //	assert(dest != NULL);
